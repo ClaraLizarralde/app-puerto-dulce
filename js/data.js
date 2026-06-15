a7c4ba1f02fdf0da08bcc2d64daa7d8bc703b268
unknown <claralizarralde.s@gmail.com>
2026-06-15 19:47:16 -0300

    fix firebase

--- js/data.js
+++ js/data.js
@@ -257,9 +257,10 @@ function guardar() {
     const {db} = window._fb;
     import('https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js')
     .then(({doc, setDoc}) => {
-     const ref = doc(db, 'locales', datos.localId, 'datos', 'main');
-const datosLimpios = JSON.parse(JSON.stringify(datos));
-setDoc(ref, datosLimpios).catch(e => console.warn('Firebase save error:', e));
+      const ref = doc(db, 'locales', datos.localId, 'datos', 'main');
+      const datosLimpios = JSON.parse(JSON.stringify(datos));
+      setDoc(ref, datosLimpios).catch(e => console.warn('Firebase save error:', e));
+    });
   }
 }
 
